62762673e18921122e76149f7881fce256b8ed21
Airton <airton.mamanih@unmsm.edu.pe>
2026-05-26 13:19:57 -0500

    Agregar funcion despedida

---
+++ oso-prime/hello.py
@@ -0,0 +1,10 @@
+def saludar(nombre):
+    return f"Hola, {nombre}!"
+
+def despedirse(nombre):
+    return f"Adiós, {nombre}!"
+
+if __name__ == "__main__":
+    nombre_usuario = input("¿Cómo te llamas? ")
+    mensaje = saludar(nombre_usuario)
+    print(mensaje)
